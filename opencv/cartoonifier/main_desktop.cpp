6345ee6eb84cc34428fd7cff7b1c041e93da5dd1
Alan Chiao <alanchiao@alanchiao-macbookpro.roam.corp.google.com>
2017-12-02 19:10:55 -0800

    helloworld and initial cartoonifier from Mastering OpenCV

---
+++ opencv/cartoonifier/main_desktop.cpp
@@ -0,0 +1,45 @@
+#include "opencv2/opencv.hpp"
+
+#if !defined VK_ESCAPE
+    #define VK_ESCAPE 0x1B
+#endif
+
+int main(int argc, char *argv[])
+{
+    int cameraNumber = 0;
+    if (argc > 1)
+        cameraNumber = atoi(argv[1]);
+
+    cv::VideoCapture camera(cameraNumber);
+    if (!camera.isOpened()) {
+        std::cerr << "ERROR: Could not access the camera or video!" << std::endl;
+        exit(1);
+    }
+
+    camera.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+    camera.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+
+    while (true)
+    {
+        cv::Mat frame;
+        camera >> frame;
+        if (frame.empty()) {
+            std::cerr << "ERROR: Could not grab a camera frame." <<
+            std::endl;
+            exit(1);
+        }
+
+        cv::Mat displayed(frame.size(), CV_8UC3);
+
+        // cartoonifyImage(frame, displayed);
+
+        imshow("Cartoonifier", frame);
+
+        char keypress = cv::waitKey(1000/50);
+        if (keypress == VK_ESCAPE) {
+            break;
+        }
+    }
+
+    return 0;
+}
